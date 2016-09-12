a912dfae177a03653ba12b1c671c2d31d8894c81
ilyablinov <ilya@Mac-Pro-ilyablinov.local>
2016-09-12 07:23:41 -0700

    create new class ‘IBServerManager’

---
+++ VkAppTest/IBServerManager.h
@@ -0,0 +1,13 @@
+//
+//  IBServerManager.h
+//  VkAppTest
+//
+//  Created by ilyablinov on 12.09.16.
+//  Copyright (c) 2016 IB. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IBServerManager : NSObject
+
+@end
---
+++ VkAppTest/IBServerManager.m
@@ -0,0 +1,13 @@
+//
+//  IBServerManager.m
+//  VkAppTest
+//
+//  Created by ilyablinov on 12.09.16.
+//  Copyright (c) 2016 IB. All rights reserved.
+//
+
+#import "IBServerManager.h"
+
+@implementation IBServerManager
+
+@end
