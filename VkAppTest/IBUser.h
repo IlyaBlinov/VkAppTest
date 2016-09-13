ca57af68f55285436edffb2d06629b3cd5bda6ed
ilyablinov <ilya@Mac-Pro-ilyablinov.local>
2016-09-13 06:17:21 -0700

    Project: CREATE : CLASS: ‘IBUser’

---
+++ VkAppTest/IBUser.h
@@ -0,0 +1,13 @@
+//
+//  IBUser.h
+//  VkAppTest
+//
+//  Created by ilyablinov on 13.09.16.
+//  Copyright (c) 2016 IB. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IBUser : NSObject
+
+@end
---
+++ VkAppTest/IBUser.m
@@ -0,0 +1,13 @@
+//
+//  IBUser.m
+//  VkAppTest
+//
+//  Created by ilyablinov on 13.09.16.
+//  Copyright (c) 2016 IB. All rights reserved.
+//
+
+#import "IBUser.h"
+
+@implementation IBUser
+
+@end
